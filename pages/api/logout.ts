76250e189429dcbfded6159a2bd5f9d9d4f5d972
JanisEglis <158576786+JanisEglis@users.noreply.github.com>
2025-10-06 23:00:53 +0300

    logout.ts

---
+++ pages/api/logout.ts
@@ -0,0 +1,6 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse){
+  res.setHeader("Set-Cookie", ['auth=; Path=/; Max-Age=0; HttpOnly; SameSite=Lax']);
+  res.redirect("/_login");
+}
